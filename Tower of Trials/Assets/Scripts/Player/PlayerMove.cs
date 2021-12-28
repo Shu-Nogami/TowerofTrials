504515dfed2df25fbe3bb8de7776cf5829abf4e4
Shu-Nogami <g2053930@cc.kyoto-su.ac.jp>
2021-12-29 01:39:44 +0900

    creat Playermove.cs

--- Tower of Trials/Assets/Scripts/Player/PlayerMove.cs
+++ Tower of Trials/Assets/Scripts/Player/PlayerMove.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class PlayerMove : MonoBehaviour
 {
+    private bool ismove = true;
+    private Vector3 nextmovepotion;
+    private Vector3 xaxismove = new Vector3(1f, 0, 0);
+    private Vector3 zaxismove = new Vector3(0, 0, 1f);
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +17,27 @@ public class PlayerMove : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (ismove)
+        {
+            SetMovePotion();
+        }
+        move();
+    }
+    void SetMovePotion(){
+        if(Input.GetKeyDown(KeyCode.UpArrow)){
+            nextmovepotion = transform.position + zaxismove;
+        }
+        if(Input.GetKeyDown(KeyCode.DownArrow)){
+            nextmovepotion = transform.position - zaxismove;
+        }
+        if(Input.GetKeyDown(KeyCode.RightArrow)){
+            nextmovepotion = transform.position + xaxismove;
+        }
+        if(Input.GetKeyDown(KeyCode.LeftArrow)){
+            nextmovepotion = transform.position - xaxismove;
+        }
+    }
+    void move(){
+        transform.position = Vector3.MoveTowards(transform.position, nextmovepotion, 3f);
     }
 }
